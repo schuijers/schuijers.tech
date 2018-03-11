76568167b764cb5a20a5f440ac6f9631b6f0892c
Martijn Schuijers <martijnschuijers@gmail.com>
2018-03-11 21:40:56 +0100

    Add icons

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,8 +1,11 @@
+/* eslint-disable global-require, import/no-extraneous-dependencies */
 module.exports = {
   plugins: [
-    require('postcss-import')(),
+    require('postcss-import')({
+      plugins: [require('stylelint')()],
+    }),
     require('rucksack-css')({
-      autoprefixer: true
+      autoprefixer: true,
     }),
     require('css-mqpacker')(),
     require('postcss-reporter')({
---
+++ .posthtmlrc.js
@@ -0,0 +1,34 @@
+const GENERATE_FAV_ICONS = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  plugins: {
+    'posthtml-favicons': {
+      configuration: {
+        path: '/',
+        appName: 'Schuijers.tech',
+        appDescription: null,
+        developerName: 'Martijn Schuijers',
+        developerURL: 'https://schuijers.tech',
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+        logging: false,
+        icons: {
+          android: GENERATE_FAV_ICONS,
+          appleIcon: GENERATE_FAV_ICONS,
+          appleStartup: GENERATE_FAV_ICONS,
+          coast: GENERATE_FAV_ICONS,
+          favicons: GENERATE_FAV_ICONS,
+          firefox: GENERATE_FAV_ICONS,
+          windows: GENERATE_FAV_ICONS,
+          yandex: GENERATE_FAV_ICONS,
+        },
+      },
+    },
+  },
+};
